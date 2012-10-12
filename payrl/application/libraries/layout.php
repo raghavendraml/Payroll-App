d68ea312d68e5492167bfbb7e196c9722471e6f3
Raghu ml <raghavendra.ml@myshoreitsolutions.co.in>
2012-10-12 20:41:23 +0530

    Commented Extra

--- payrl/application/libraries/layout.php
+++ payrl/application/libraries/layout.php
@@ -13,7 +13,7 @@ class Layout {
    public function view($view, $data=null, $return=false){
    	  $output = null;
       $this->obj->load->view('include/header');	
-      log_message('error',"SEGMENT: ".$this->obj->uri->rsegment(2));
+      //log_message('error',"SEGMENT: ".$this->obj->uri->rsegment(2));
       /* Save the current URL in browser to variable */
       $cur_url = $this->obj->uri->uri_string();
    	  if($this->obj->session->userdata('logged_in'))
@@ -27,7 +27,7 @@ class Layout {
         if ($this->obj->uri->rsegment(2) != 'logout' && $this->obj->uri->rsegment(2) != 'login')
         {
           $this->obj->session->set_userdata(array('last_visit'=>$cur_url));
-          log_message('error',"LAST VIS: ".$this->obj->session->userdata('last_visit'));
+          //log_message('error',"LAST VIS: ".$this->obj->session->userdata('last_visit'));
         }
         
       }
